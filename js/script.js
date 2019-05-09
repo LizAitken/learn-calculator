d8336ea89d4abce3c34b21d5606d7f5190afaa1d
Liz Aitken <liz.a.aitken@gmail.com>
2019-05-09 13:12:19 -0400

    added calculations to result event handler

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ number.forEach(function(numbers) {
     numbers.addEventListener('click', function(){
         // console.log(this.innerHTML);
         let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
         if (resultDisplayed === false) {
             input.innerHTML += this.innerHTML;
         } else if (
@@ -54,8 +55,50 @@ operator.forEach(function(operators) {
 
 // on click of 'equal' button
 result.addEventListener('click', function(){
-    console.log(this.innerHTML);
+    const currentString = input.innerHTML;
+    const numberStringArray = currentString.split(/\+|\-|\×|\÷/g);
+    let numbersArray = [];
+    numberStringArray.forEach(function(number) {
+        numbersArray.push(Number(number));
+    });
+
+    const operatorsArray = currentString.replace(/[0-9]|\./g, "").split("");
+    let multiply = operatorsArray.indexOf('×');
+    console.log(numbersArray);
+    while (multiply != -1) {
+        numbersArray.splice(multiply, 2, numbersArray[multiply] * numbersArray[multiply +1]);
+        operatorsArray.splice(multiply, 1);
+        multiply = operatorsArray.indexOf('×');
+    }
+
+    let divide = operatorsArray.indexOf('÷');
+    while(divide != -1) {
+        numbersArray.splice(divide, 2, numbersArray[divide] / numbersArray[divide +1]);
+        operatorsArray.splice(divide, 1);
+        divide = operatorsArray.indexOf('÷');
+    }
+
+    let add = operatorsArray.indexOf('+');
+    while(add != -1) {
+        numbersArray.splice(add, 2, numbersArray[add] + numbersArray[add +1]);
+        operatorsArray.splice(add, 1);
+        add = operatorsArray.indexOf('+');
+    }
+
+    let subtract = operatorsArray.indexOf('-');
+    while(subtract != -1) {
+        numbersArray.splice(subtract, 2, numbersArray[subtract] - numbersArray[subtract +1]);
+        operatorsArray.splice(subtract, 1);
+        subtract = operatorsArray.indexOf('-');
+    }
+
+    resultDisplayed = true; 
+    input.innerHTML = numbersArray;
 });
+
+
+
+
 
 
 // clearing the input on press of clear
