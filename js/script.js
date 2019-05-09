843be5550b7dc323761a7bec60a2791d9e198b37
Liz Aitken <liz.a.aitken@gmail.com>
2019-05-09 10:41:13 -0400

    Added conditonal for the numbers to check if a result has been displayed.

--- js/script.js
+++ js/script.js
@@ -17,19 +17,32 @@ let resultDisplayed = false; // flag to keep an eye on what output is displayed
 number.forEach(function(numbers) {
     numbers.addEventListener('click', function(){
         // console.log(this.innerHTML);
-        input.innerHTML += this.innerHTML;
+        let currentString = input.innerHTML;
+        if (resultDisplayed === false) {
+            input.innerHTML += this.innerHTML;
+        } else if (
+            resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === '×' || lastChar === '÷' 
+            ) {
+                resultDisplayed = false;
+                input.innerHTML += this.innerHTML;
+        } else {
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += this.innerHTML;
+        }
     });
 });
+
 
 // adding click handlers to the operation buttons
 operator.forEach(function(operators) {
     operators.addEventListener('click', function(){
-        // console.log(this.innerHTML);
-        let lastChar = input.innerHTML[(input.innerHTML).length - 1];
-        if (lastChar === '+' ||lastChar === '-' || lastChar === '×' || lastChar === '÷') {
-            let newString = (input.innerHTML).substring(0, (input.innerHTML).length - 1) + e.target.innerHTMl;
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
+        if (lastChar === '+' || lastChar === '-' || lastChar === '×' || lastChar === '÷') {
+            let newString = currentString.substring(0, currentString.length - 1) + this.innerHTML;
             input.innerHTML = newString;
-        } else if ((input.innerHTML).length == 0) {
+        } else if (currentString.length == 0) {
             console.log('enter a number first');
         } else {
             input.innerHTML += this.innerHTML;
